e807a354d4dc27ce9d8c4c7003957ae9228e300a
Yuvia Leon <ykl2112@columbia.edu>
2023-08-15 12:50:54 -0500

    added route and nav link for creating a listing

--- auctions/views.py
+++ auctions/views.py
@@ -61,3 +61,7 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+def create(request):
+    return render(request, "auctions/create.html")
