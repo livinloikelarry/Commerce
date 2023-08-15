b114eb74dec84c3d9fe20cda572960e744fd88d1
Yuvia Leon <ykl2112@columbia.edu>
2023-08-15 18:17:19 -0500

    added superuser and tweaked models

--- auctions/models.py
+++ auctions/models.py
@@ -3,8 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    watchlist = models.ForeignKey(
-        'Listing', on_delete=models.SET_NULL,  null=True)
+    watchlist = models.ManyToManyField('Listing', blank=True)
 
     def __str__(self):
         return f"{self.username}"
